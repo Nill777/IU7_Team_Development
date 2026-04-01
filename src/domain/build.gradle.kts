3dd0030a91271d002329b078348e3adef7ebb109
pan22u263 <palamarchukan@student.bmstu.ru>
2026-04-01 18:05:46 +0300

    feat(PROJ-3): multi-module arch is configured

---
+++ src/domain/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    config.setFrom(files("$rootDir/detekt.yml"))
+    buildUponDefaultConfig = true
+    autoCorrect = true
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
