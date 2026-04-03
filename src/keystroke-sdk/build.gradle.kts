04688e0a90777aad38688ce8adbfb8807b225aed
pan22u263 <palamarchukan@student.bmstu.ru>
2026-04-03 15:34:53 +0300

    feat(PROJ-5): IMService was created with Jetpack Compose

--- src/keystroke-sdk/build.gradle.kts
+++ src/keystroke-sdk/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.kbk.keystroke_sdk"
+    namespace = "com.kbk.keystrokeSdk"
     compileSdk = 36
 
     defaultConfig {
@@ -25,11 +25,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
