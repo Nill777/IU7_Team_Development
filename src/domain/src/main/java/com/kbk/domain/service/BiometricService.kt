69cac243583960bc3702f6641b93de006a1c425b
pan22u263 <palamarchukan@student.bmstu.ru>
2026-04-11 00:56:04 +0300

    feat(PROJ-7): IBiometricService added

--- src/domain/src/main/java/com/kbk/domain/service/BiometricService.kt
+++ src/domain/src/main/java/com/kbk/domain/service/BiometricService.kt
@@ -1,12 +1,13 @@
 package com.kbk.domain.service
 
 import com.kbk.domain.irepository.IBiometricRepository
+import com.kbk.domain.iservice.IBiometricService
 import com.kbk.domain.models.BiometricSample
 
 class BiometricService(
     private val repository: IBiometricRepository
-) {
-    suspend fun processAndSaveSample(sample: BiometricSample) {
+) : IBiometricService {
+    override suspend fun processAndSaveSample(sample: BiometricSample) {
         repository.saveSample(sample)
     }
 }
